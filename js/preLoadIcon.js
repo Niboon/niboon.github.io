bd8100325caab230ba1cd48335cefc1b711a615e
Niboon Tangnirunkul <ntangnir@uwaterloo.ca>
2015-02-24 18:29:16 -0500

    Removed Fade in

--- js/preLoadIcon.js
+++ js/preLoadIcon.js
@@ -5,8 +5,6 @@ jQuery(document).ready(function() {
     //grab the loading icon screen
     var loading = jQuery(".loadicon");
 
-    loading.fadeTo(150, 0.99);
-
 //remove the loading icon screen when page is ready
     jQuery(window).ready(function () {
         loading.delay(250).fadeOut(700);
