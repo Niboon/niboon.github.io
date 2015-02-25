b16d61a8b702551ac9709bb62e33627e4728a1ab
Niboon Tangnirunkul <ntangnir@uwaterloo.ca>
2015-02-24 23:58:13 -0500

    Moving script

--- js/defer.js
+++ js/defer.js
@@ -2,6 +2,9 @@
  * Created by Niboon on 2015-02-24.
  */
 (function($) {
+    $.ajaxSetup({
+        cache: true
+    });
     $.getScript("jquery.dropotron.min.js");
     $.getScript("skel.min.js");
     $.getScript("skel-layers.min.js");
