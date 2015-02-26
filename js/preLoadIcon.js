3081dd580be7c91e728fb5d1f9293865c1b141e7
Niboon Tangnirunkul <ntangnir@uwaterloo.ca>
2015-02-26 01:49:28 -0500

    Bind fade preload icon to skel layers showend

--- js/preLoadIcon.js
+++ js/preLoadIcon.js
@@ -2,11 +2,11 @@
  * Created by Niboon on 2015-02-24.
  */
 jQuery(document).ready(function() {
+    var layer = skel.plugins.layers.get('navPanel');
     //grab the loading icon screen
     var loading = jQuery(".loadicon");
-
-//remove the loading icon screen when page is ready
-    jQuery(window).ready(function () {
+    layer.on('showend', function() {
+        //remove the loading icon screen when layers are ready
         loading.delay(250).fadeOut(600);
     });
 });
